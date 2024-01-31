81fc2494e26f42c664f2f86117e2e725e1fda419
GavinRanasinghe29870 <gavinkisara@gmail.com>
2024-01-31 10:27:11 +0530

    Add project files.

---
+++ LAB 01/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question1();
+            Question2();
+            Question3();
+            Question4();
+            Question5();
+            Console.ReadLine();
+        }
+            //QUESTION 01
+            static void Question1()
+            {
+                Console.WriteLine("Enter Length");
+                int l = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Width");
+                int w = Convert.ToInt32(Console.ReadLine());
+
+                AreaCal(l, w);
+            }
+
+            static void AreaCal(int width, int height)
+            {
+                int area;
+                area = width * height;
+                Console.WriteLine("The area is: " + area);
+            }
+
+            //QUESTION 02
+
+            static void Question2()
+            {
+                int[] num = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Enter number {0}", i);
+                    if (int.TryParse(Console.ReadLine(), out num[i]))
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invlaid input");
+                        i--;
+                    }
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (num[i] % 2 == 0)
+                    {
+                        Console.Write("Number {0}", num[i]);
+                        Console.WriteLine(" is Even");
+                    }
+                    else
+                    {
+                        Console.Write("Number {0}", num[i]);
+                        Console.WriteLine(" is Odd");
+                    }
+                }
+            }
+
+            //QUESTION 03
+
+            static void Question3()
+            {
+                int sum = 0;
+                Console.WriteLine("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("ERROR");
+                }
+                else
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine("Sum = {0}", sum);
+                }
+
+            }
+
+            //QUESTION 04
+            static void Question4()
+            {
+                Console.WriteLine("Enter number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write(Fibonacci(i) + " ");
+                }
+            }
+
+            static int Fibonacci(int n)
+            {
+                if (n <= 1)
+                {
+                    return n;
+                }
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+
+            //Question 05
+            static void Question5()
+            {
+                Console.WriteLine("Enter number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Multiplication table end number: ");
+                int endNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                for (int i = 0; i < endNum; i++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i + 1, num, (i + 1) * num);
+                }
+            }
+
+        }
+    }
+
