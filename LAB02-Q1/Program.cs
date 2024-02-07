e592d779e71511fa9ee95759d45a26699daac540
GavinRanasinghe29870 <gavinkisara@gmail.com>
2024-02-07 14:20:21 +0530

    LAB02-Q1 added

---
+++ LAB02-Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB02_Q1
+{
+    internal class Program
+    {
+        class Books
+        {
+            public String Author;
+            public String Title;
+        }
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.Author = "AnitaDesi";
+            books.Title = "Village by sea";
+            Console.WriteLine("Title: " + books.Title);
+            Console.WriteLine("Author: " + books.Author);
+            Console.ReadLine();
+        }
+    }
+}
