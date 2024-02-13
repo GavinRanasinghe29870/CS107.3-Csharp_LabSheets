bf5ded60f26476620844a7e20053209558d35374
GavinRanasinghe29870 <gavinkisara@gmail.com>
2024-02-13 20:18:48 +0530

    Lab 02- Q2

---
+++ LAB02-Q2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2q2
+{
+
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public double Balance { get; private set; }
+
+
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Successfully deposited ${amount}. Updated balance: ${Balance}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount. Please deposit a positive amount.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            BankAccount account = new BankAccount("123456789", 1000);
+
+
+            account.Deposit(500);
+
+            Console.WriteLine($"Current balance: ${account.Balance}");
+            Console.ReadLine();
+        }
+    }
+
+}
