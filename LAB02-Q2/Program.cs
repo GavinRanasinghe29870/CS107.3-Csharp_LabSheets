3d3eceeb668f5828dcebec1f78390517fe4a31ea
GavinRanasinghe29870 <gavinkisara@gmail.com>
2024-02-23 15:16:22 +0530

    Q3 added

--- LAB02-Q2/Program.cs
+++ LAB02-Q2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab2q2
+namespace LAB02_Q2
 {
 
     class BankAccount
---
+++ LAB02-Q3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB02_Q3
+{
+
+
+    class TemperatureTracker
+    {
+        private double[] weeklyTemperatures;
+
+
+        public TemperatureTracker(int daysOfWeek)
+        {
+            weeklyTemperatures = new double[daysOfWeek];
+        }
+
+
+        public void InputTemperatures()
+        {
+            for (int i = 0; i < weeklyTemperatures.Length; i++)
+            {
+                Console.Write($"Enter temperature for day {i + 1}: ");
+                weeklyTemperatures[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+
+        public void DisplayWeeklyTemperatureReport()
+        {
+            Console.WriteLine("\nWeekly Temperature Report:");
+            for (int i = 0; i < weeklyTemperatures.Length; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {weeklyTemperatures[i]}");
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            int daysOfWeek = 7;
+
+
+            TemperatureTracker tracker = new TemperatureTracker(daysOfWeek);
+
+
+            tracker.InputTemperatures();
+
+
+            tracker.DisplayWeeklyTemperatureReport();
+        }
+    }
+
+}
